47ea86a2259432f0246fc3975e320cbb7cb75f1b
Filter <not.so.filter@hotmail.com>
2025-05-05 17:18:42 -0400

    rename build_tools to tool

--- chkbitperfect.bat
+++ chkbitperfect.bat
@@ -1,4 +1,4 @@
-call build_tools/md5.bat s1built.bin md5
+call tool/md5.bat s1built.bin md5
 if "%md5%" equ "b06578c3412de1c5be765a59dec5ff1f" (
       echo MD5 identical!
 ) else (
--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
 IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL
-build_tools\asw -xx -q -A -L -U -E -i . main.asm
-build_tools\p2bin main.p -p=FF s1built.bin
+tool\asw -xx -q -A -L -U -E -i . main.asm
+tool\p2bin main.p -p=FF s1built.bin
 del main.p
