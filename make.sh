08509fc9f6df0be3fd823b4892130e8dc3140188
Filter <not.so.filter@hotmail.com>
2025-05-25 14:44:30 -0400

    change assemblers again

--- make.sh
+++ make.sh
@@ -3,5 +3,5 @@ if test -f s1built.bin; then
 	mv -f s1built.bin s1built.prev.bin
 fi
 ./tool/linux/asl -xx -q -A -L -U -E -i . main.asm
-./tool/linux/p2bin main.p s1built.bin
+./tool/linux/p2bin -p=FF main.p s1built.bin
 rm -f main.p
