4306c0235f3ac6ca7f5e79d2bff6b376aca9af08
Filter <not.so.filter@hotmail.com>
2025-05-25 13:57:59 -0400

    attempt to fix linux build script part uno

--- make.sh
+++ make.sh
@@ -2,6 +2,6 @@
 if [-f s1built.bin]; then
 	mv -f s1built.bin s1built.prev.bin
 fi
-tool/linux/asl -xx -q -A -L -U -E -i . main.asm
-tool/linux/p2bin main.p s1built.bin
+./tool/linux/asl -xx -q -A -L -U -E -i . main.asm
+./tool/linux/p2bin main.p s1built.bin
 rm -f main.p
